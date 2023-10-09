d7bee2655491cb2c36a583aa6f6e34e410f12e3d
RazielGelo <angeloguerra1986x@gmail.com>
2023-10-08 23:46:55 -0700

    modified post request api

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -5,7 +5,7 @@ import blankprofile from "../images/blank-profile.jpg"
 
 export const Card = ({ smallCard }) => {
     const cardClasses = `
-    border-2 rounded-[20px] shadow-lg
+    border-2 rounded-[20px] shadow-lg 
   
     ${smallCard ? 'w-[180px] h-[180px]' : ''}
   `;
