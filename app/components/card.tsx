c73e20fd582af56aefe29a25cb6fba0cf3bb343b
MiykelC <105033752+MiykelC@users.noreply.github.com>
2023-10-09 19:04:54 -0700

    Fixed some style of cards

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -8,58 +8,61 @@ interface CardProps {
 }
 
 export const Card = ({ smallCard }: CardProps) => {
-    
+
     return (
         <>
             {smallCard ?
-                <div className="border-2 w-[300px] h-[360px] rounded-[10px]  hover:ease-in-out duration-300  hover:border-[2px] hover:border-rose-500 ">
+                <div className="border-2 w-[300px] h-[260px] rounded-[10px] bg-neutral-900 text-white hover:ease-in-out duration-300  hover:border-[2px]  ">
+                    <div className="flex items-center justify-center mt-2">
                     <img
                         src={gaming.src}
-                        className="rounded-[5px]"
-                    />
-                    <div className="flex flex-col items-center ">
+                        className="rounded-[5px] w-[275px]"
+                    /></div>
+                    <div className="flex flex-col items-end ">
                         <img
                             src={blankprofile.src}
-                            className=" object-cover w-[80px] h-[80px] rounded-full mt-[-40px] border-4 border-white"
+                            className=" object-cover w-[50px] h-[50px] rounded-full mt-[-28px] mr-4 border-4 border-neutral-900"
 
-                        /><p>Michael Chua</p>
+                        />
                     </div>
-                    <div className="flex flex-col mt-4 gap-4 text-center items-center">
+                    <div className="flex flex-col ml-2 mt-[-20px] ">
                         <p className="text-[20px]">Coding With me</p>
-                        <p className="text-[11px]"> Technology</p>
-                        <p className="text-[11px]">MM/DD/TIME</p>
-                        <button
-                  className="text-center bg-rose-500 text-white font-bold w-[165px] text-[11px] rounded h-[25px] hover:bg-white hover:text-rose-500 hover:border-[2px] hover:border-rose-500 hover:ease-in-out duration-300">
-                  Learn more
-                </button>
+                        <p>Michael Chua</p>
+                        <p className="text-[11px] mt-2"> Technology</p>
+                        <p className="text-[11px] mt-2">October 11, 2023 at 11:30 AM</p>
+
 
                     </div>
-                    <div>
-
+                    <div className="flex justify-center mt-2">
+                        <button
+                            className="text-center bg-rose-500 text-white font-bold w-[280px] text-[11px] rounded h-[25px] hover:bg-white hover:text-rose-500 hover:border-[2px] hover:border-rose-500 hover:ease-in-out duration-300">
+                            Learn more
+                        </button>
                     </div>
                 </div>
 
 
-                : <div className="border-2 w-[420px] h-[440px] rounded-[10px] shadow-lg hover:ease-in-out duration-300  hover:border-[2px] hover:border-rose-500 ">
+                : <div className="border-2 w-[420px] h-[440px] bg-neutral-900 text-white rounded-[10px] shadow-lg hover:ease-in-out duration-300  hover:border-[2px] hover:border-rose-500 ">
+                       <div className="flex items-center justify-center mt-2">
                     <img
                         src={gaming.src}
-                        className="rounded-[5px]"
-                    />
+                        className="rounded-[5px] w-[400px]"
+                    /></div>
                     <div className="flex flex-col items-center ">
                         <img
                             src={blankprofile.src}
-                            className=" object-cover w-[80px] h-[80px] rounded-full mt-[-40px] border-4 border-white"
+                            className=" object-cover w-[80px] h-[80px] rounded-full mt-[-40px] border-4 border-neutral-900"
 
                         /><p>Michael Chua</p>
                     </div>
                     <div className="flex flex-col mt-4 gap-4 text-center items-center">
                         <p className="text-[30px]">Coding With me</p>
                         <p>Technology</p>
-                        <p>MM/DD/TIME</p>
+                        <p>October 20 2023 at 9:00 PM</p>
                         <button
-                  className="text-center bg-rose-500 text-white font-bold w-[195px] rounded h-[35px] hover:bg-white hover:text-rose-500 hover:border-[2px] hover:border-rose-500 hover:ease-in-out duration-300">
-                  Learn more
-                </button>
+                            className="text-center bg-rose-500 text-white font-bold w-[305px] rounded h-[35px] hover:bg-white hover:text-rose-500 hover:border-[2px] hover:border-rose-500 hover:ease-in-out duration-300">
+                            Learn more
+                        </button>
 
                     </div>
                     <div>
