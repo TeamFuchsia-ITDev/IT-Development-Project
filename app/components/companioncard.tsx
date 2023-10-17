7c0d4500ef50be6b1d3a0e4e1d044ad5c6c9e423
MiykelC <105033752+MiykelC@users.noreply.github.com>
2023-10-17 12:42:08 -0700

    Fixed Companion Card

--- app/components/companioncard.tsx
+++ app/components/companioncard.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { ApplicationProps } from "@/app/libs/interfaces";
+import { useState } from "react";
 
 export const CompanionCard = ({
   application,
 }: {
   application: ApplicationProps;
 }) => {
+  const [showWhy, setShowWhy] = useState(false);
+
   function calculateAge(birthdate: string) {
     const birthYear = new Date(birthdate).getFullYear();
     const currentYear = new Date().getFullYear();
@@ -15,7 +18,7 @@ export const CompanionCard = ({
   }
   return (
     <>
-      <div className="shadow-xl border-2  h-auto w-[410px] mb-4 rounded-[10px] hover:ease-in-out duration-300 ">
+      <div className=" border-2  h-auto w-[410px] mb-4 rounded-[10px] hover:ease-in-out duration-300 ">
         <div className="flex flex-row pt-4 pl-4 pr-4 pb-4">
           <img
             src={application.compImage}
@@ -25,19 +28,25 @@ export const CompanionCard = ({
             <h1>
               <p>
                 {application.compName}
-				<br />
-                <a className="text-gray-400">{calculateAge(application.compBirthday).toString()}</a>
+                <br />
+                <h1 className="text-gray-400">{application.compCity}</h1>
+                <a className="">
+                  {calculateAge(application.compBirthday).toString()}
+                </a>
               </p>
             </h1>
-            <h1 className="text-gray-400">{application.compCity}</h1>
+
             <h1 className="">{application.compEthnicity}</h1>
             <h1>
               RATE: <a className="text-rose-500">${application.amount}</a>
             </h1>
-            <h1>{application.description}</h1>
+            <h1>{showWhy ? application.description : ""}</h1>
           </div>{" "}
           <div className="flex flex-col gap-2 justify-center m-auto align-center items-center">
-            <button className="text-center bg-rose-500 text-white text-[15px] rounded-full h-[30px]  w-[100px] hover:bg-white hover:text-rose-500 hover:border-[2px] hover:border-rose-500 hover:ease-in-out duration-300">
+            <button
+              className="text-center bg-rose-500 text-white text-[15px] rounded-full h-[30px]  w-[100px] hover:bg-white hover:text-rose-500 hover:border-[2px] hover:border-rose-500 hover:ease-in-out duration-300"
+              onClick={() => setShowWhy(!showWhy)}
+            >
               why hire
             </button>
             <button className="text-center bg-green-500 text-white text-[15px] rounded-full h-[30px]  w-[100px] hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500 hover:ease-in-out duration-300">
