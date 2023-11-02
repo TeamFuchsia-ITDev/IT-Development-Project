13cc8210537e2b962828be800215af3e78d5d543
MiykelC <105033752+MiykelC@users.noreply.github.com>
2023-11-01 22:55:41 -0700

    added lapsed Card UI and fixed logout redirection

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -36,17 +36,18 @@ export const Navbar = () => {
         {/* <img src={logo.src} alt="Logo" width={60} className="m-2 " /> */}
         <h1>Serve-Ease</h1>
         <div className=" relative flex gap-5  text-sm  items-center">
-          
-          {mode === true ? (  <a
-            href="/post "
-            className={`relative group ${
-              pathname === "/post" ? isLinkActive : ""
-            }`}
-          >
-            Post Request
-            <div className="absolute left-0 w-0 h-[2px] bg-rose-500 group-hover:w-full transition-transform transform origin-left transform scale-x-0 group-hover:scale-x-100"></div>
-          </a>): null }
-        
+          {mode === true ? (
+            <a
+              href="/post "
+              className={`relative group ${
+                pathname === "/post" ? isLinkActive : ""
+              }`}
+            >
+              Post Request
+              <div className="absolute left-0 w-0 h-[2px] bg-rose-500 group-hover:w-full transition-transform transform origin-left transform scale-x-0 group-hover:scale-x-100"></div>
+            </a>
+          ) : null}
+
           <a
             href="/profilepage"
             className={`relative group ${
@@ -127,11 +128,7 @@ export const Navbar = () => {
                   <a
                     className="block px-4 py-2 cursor-pointer hover:bg-gray-100 hover:text-red-500"
                     onClick={() => {
-                    //   router.push("/login");
-                    //   setTimeout(() => {
-                    //     signOut();
-                    //   }, 500);
-					signOut()
+                      signOut({ redirect: false, callbackUrl: "/login" });
                     }}
                   >
                     Sign Out
