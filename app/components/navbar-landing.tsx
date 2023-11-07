024cee7e4510194f89b16030ad6761de5358d509
MiykelC <105033752+MiykelC@users.noreply.github.com>
2023-11-06 21:18:40 -0800

    Started working on the second part of the landing page

--- app/components/navbar-landing.tsx
+++ app/components/navbar-landing.tsx
@@ -10,7 +10,7 @@ import serveease from "@/app/images/Serve-ease.svg";
 
 export const NavbarLanding = () => {
   return (
-    <main className="mb-4 mt-4" >
+    <main className=" mt-4" >
       <div className="flex flex-row justify-between items-center pl-4 pr-4 pb-4">
         <div>
           <img src={serveease.src} alt="Serve-Ease" width={200} />
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { User } from "./components/user";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 import { NavbarLanding } from "./components/navbar-landing";
+import ImageLandings from "@/app/images/ImageLanding.png";
+import blobimage from "@/app/images/blob.png";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -43,34 +45,39 @@ export default async function Home() {
     //     </Link>
     //   </div>
     // </section>
-    <div >
+    <div>
       <div className="">
-      <NavbarLanding />
+        <NavbarLanding />
       </div>
-      <div className="pl-24 pr-24">
-        <div className="flex  h-screen justify-center items-center">
-          <div className="pb-[200px] text-center">
-            <h1 className="text-[30px]">
-              JOIN THE <span className="text-blue-600">SERVE-EASE</span>{" "}
+      <div className="relative">
+        <div className="flex h-[650px] justify-center items-center">
+          <div className=" text-center">
+            <img
+              src={ImageLandings.src}
+              alt="Serve-Ease"
+              className="absolute z-[-1] top-0 h-[680px] left-0 w-screen object-cover"
+            />
+            <h1 className="text-[30px] text-white">
+              JOIN THE <span className="text-blue-500 ">SERVE-EASE</span>{" "}
               COMMUNITY TODAY
             </h1>
-            <h1 className="text-[30px]">
+            <h1 className="text-[30px] text-white">
               {" "}
               AND START HELPING OR START GIVING OPPORTUNITIES
             </h1>
-            <p className="text-[18.5px]">
-              Become a <span className="text-red-500">Companion</span> to lend a
-              helping hand or be a{" "}
+            <p className="text-[18.5px] text-white">
+              Become a <span className="text-red-500 ">Companion</span> to lend
+              a helping hand or be a{" "}
               <span className="text-blue-500">Requester</span> to seek for help,
               sign up today.
             </p>
             <div className="flex flex-row gap-2  justify-center mt-5">
               <Link href={`/register`}>
-                <button className="bg-blue-500 text-white font-bold text-[15px] rounded w-[300px] h-[45px] hover:bg-white hover:text-blue-500 hover:border-[2px] hover:border-blue-500 hover:ease-in-out duration-300">
-                 Sign up free!
+                <button className="bg-blue-500 text-white font-bold text-[15px] rounded w-[300px] h-[45px] hover:bg-white hover:text-blue-500 hover:border-[3px] hover:border-blue-500 hover:ease-in-out duration-300">
+                  Sign up free!
                 </button>
               </Link>
-              <p className="flex justify-center items-center gap-1">
+              <p className="flex justify-center items-center gap-1 text-white">
                 Already have an account?{" "}
                 <a href="/login" className="text-blue-500 text-center">
                   {" "}
@@ -80,13 +87,19 @@ export default async function Home() {
             </div>
           </div>
         </div>
-        <div className="flex flex-row border-2 w-[100%] ">
-          <div className="w-1/2">
-
+        <div className="flex flex-row h-[650px] w-[100%] justify-center items-center gap-12 pl-24 mt-32">
+          <div className="w-1/2 flex flex-col ">
+            <h1 className="text-[60px]">Why use our App?</h1>
+            <p className="text-xl ">
+            We create a platform that helps connects individuals who are seeking for help to complete specific task that they cant do alone. It also helps individuals whenever they’re just lonely and need someone just to be their companion. its free of use everyone could either be a companion or a requester. Our main goal for this platform is to ensure users will have someone to lean on in times of loneliness, someone to be with just to have fun or simple have someone to chat with
+            </p>
           </div>
-          <div className="w-1/2">
-
+          <div className="w-1/2 ">
+            <img src={blobimage.src} alt="" className="w-[600px]" />
           </div>
+        </div>
+        <div className="flex w-[100%]">
+
         </div>
       </div>
     </div>
